5aae999ecde5c19601da1ca3248424aa86eaf489
benoit-planche <benoit.planche@etu.umontpellier.fr>
2024-07-01 17:02:52 +0200

    feat: Update topic-posts component HTML and styles
    This commit updates the HTML and styles of the topic-posts component. The changes include:
    - Adding a container div to wrap the content
    - Displaying the topic title and description
    - Listing the posts with their titles and content
    - Adding a button to create a new post
    
    These updates improve the layout and functionality of the topic-posts component.

--- src/app/topic-posts/topic-posts.component.ts
+++ src/app/topic-posts/topic-posts.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { PostsService } from '../posts.service';
+import { TopicsService } from '../topics.service';
 
 @Component({
-  selector: 'app-topic-posts',
   standalone: true,
-  imports: [],
+  selector: 'app-topic-posts',
+  imports: [RouterLink, CommonModule], 
   templateUrl: './topic-posts.component.html',
-  styleUrl: './topic-posts.component.scss'
+  styleUrls: ['./topic-posts.component.scss'],
+  providers: [PostsService, TopicsService],
 })
-export class TopicPostsComponent {
-topics: any;
+export class TopicPostsComponent implements OnInit {
+  topicId!: number;
+  topic: any;
+  posts: any[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private topicsService: TopicsService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.topicId = +params['id'];
+      this.topic = this.topicsService.getTopicById(this.topicId);
+      this.posts = this.postsService.getPostsByTopicId(this.topicId);
+    });
+  }
 }
